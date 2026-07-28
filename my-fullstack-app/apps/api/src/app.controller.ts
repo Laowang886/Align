7b068a4ad17924f12b2a2ad9bf0b722bef86196a
Renbo <renbo@ucsb.edu>
2026-07-28 13:02:32 -0700

    add health check endpoint

--- my-fullstack-app/apps/api/src/app.controller.ts
+++ my-fullstack-app/apps/api/src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { SkipThrottle } from '@nestjs/throttler';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @SkipThrottle()
+  @Get('health')
+  health() {
+    return {
+      status: 'ok',
+    };
   }
 }
